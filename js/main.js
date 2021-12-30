603334fd6fe421055f07919d7ecc6a8aeebe2bee
Dmitriy <osendiesesein@gmail.com>
2021-12-30 15:07:08 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// let a, b, c, P,S;
+// a = +prompt('Введите первый катет');
+// b = +prompt('Введите второй катет');
+
+// P = (sqrt(a**2 + b**2 ) + a + b);
+// c = P - (a + b);
+// S = (a/2) * b;
+
+// alert('Площадь равна = '+ S, 'Периметр равен = '+ P, 'Гипотенуза равна = '+ c)
+
+
+
+
+// let first, second, third;
+// first = +prompt('')
+// second = +prompt('')
+// third = +prompt('')
+
+// if (first > second){
+//     if ( first > third){
+//         alert('Первое число ('+ first +') самое большое')
+//     }
+//     else{
+//         alert('Третье число ('+ third +') самое большое')
+//     }
+// }
+// else{
+//     if (second > third){
+//         alert('Второе число ('+ second +') самое большое')
+//     }
+//     else{
+//         alert('Третье число ('+ third +') самое большое')
+//     }
+// }
+
+
+
+let even;
+even = +prompt('')
+
+if (!(even % 2)){
+    alert('Число четное')
+}
+else{
+    alert('Число нечетное')
+}
