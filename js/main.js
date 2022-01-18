883852c8435acdecc119dbf6864653ee38bac117
Dmitriy <osendiesesein@gmail.com>
2022-01-18 17:02:44 +0300

    dz

--- js/main.js
+++ js/main.js
@@ -88,33 +88,88 @@
 
 
 // Дана строка. Определите общее количество символов '+' и '-' в ней. А так же сколько таких символов, после которых следует цифра ноль.
-a = ('abc0-+');
-let n = 0;
-let p = 0;
+// a = ('abc0-+');
+// let n = 0;
+// let p = 0;
 
-for (let i=0; i<a.length; i++){
-    if (a[i] == '+'){
-        n++
+// for (let i=0; i<a.length; i++){
+//     if (a[i] == '+'){
+//         n++
         
-    }
-    if (a[i] == '-'){
-        p++
+//     }
+//     if (a[i] == '-'){
+//         p++
         
-    }
+//     }
     
-    if (a[i] == '0'){
-        k = a.indexOf('0')
-        k1 = a.slice(0,k)
-        k2 = k1.slice(-1)
-        console.log(k2)
+//     if (a[i] == '0'){
+//         k = a.indexOf('0')
+//         k1 = a.slice(0,k)
+//         k2 = k1.slice(-1)
+//         console.log(k2)
+//     }
+//     else{
+//         console.log('нулей нет')
+//     }
+// }
+// console.log(p+n)
+
+// 1 задание Удалите в строке все символы '!'
+
+// let str = 'asd!asdf!adf!asdf!!!!'
+// str = str.split('!').join('');
+// alert(str);
+
+//Задача 2 Дана строка, состоящая из слов, разделенных пробелами.
+
+// Определите количество слов в строке.
+// В данной строке вставить после каждого символа 'a' символ 'b'.
+// Удалите 3 первых слова
+// let n;
+// let str = 'Larem ipsum cillum laborum'
+
+// alert(str.split(' ').length)
+// alert(str.replace('a','ab'))
+
+// len = str.length;
+// let element = '!'; index;
+
+// for ( let i = 0; i < len; i++){
+//     index = indexOf(' ')
+//     alert(index)
+// }
+// alert(len)
+
+
+// Задача 3
+// email = 'admin@email.com'
+// Дан email в строке. Определить, является ли он корректным
+
+// наличие символа '@' и точки
+// наличие не менее двух символов после последней точки
+
+let main = 'admin@email.com'; j = 0; i = 0;
+len = main.length;
+
+while (i != '@'){
+    if(main[i] == '@'){
+        while(j != '.'){
+            if(main[j] == '.'){
+                alert('Почта введена правильно')
+            }
+            else{
+                alert('Почта введена неправильно')
+                j++
+            }
+            
+            break;
+        }
+    
     }
-    else{
-        console.log('нулей нет')
-    }
+    i++;
+    break;
+
 }
-console.log(p+n)
-
-
 
 
 // все методы
